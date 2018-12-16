13a2a25d9b965a51525eaf0fb9a0fbd901132fdf
Julio Silva <julio.sguz@gmail.com>
2018-12-16 12:45:45 -0400

    Add nodemon and cuse set to update a document

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const admin = require('firebase-admin')
+const serviceAccountKey = require(`./keys/${process.env.FIREBASE_SERVICE_ACCOUNT}`)
+
+// Configuration
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey)
+})
+
+const firestoreDB = admin.firestore()
+firestoreDB.settings({ timestampsInSnapshots: true })
+
+console.log('Timezone:: ', process.env.TZ)
+
+const updateUser = async (userId, newData) => {
+  await firestoreDB
+    .collection('users')
+    .doc(userId)
+    .set(newData, { merge: true })
+
+  const response = await firestoreDB
+    .doc(`users/${userId}`)
+    .get()
+
+  console.log('response.id', response.id)
+  console.log('response.data', response.data())
+}
+
+updateUser('w8PvMsgiMBjPibioHND8', {
+  updateDate: (new Date()).toISOString()
+})
